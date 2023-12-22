8f29264ff62b587e53e0de68583a568f963ed91f
Jeason <me@cowpoke.cc>
2023-12-22 14:13:43 +0800

    test: add github actions

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
-const { createConfig } = require('@umijs/test');
 // const transform = [require.resolve('ts-jest'), { diagnostics: false }];
 const transform = [
   require.resolve('@swc/jest'),
@@ -16,9 +15,18 @@ const transform = [
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  ...createConfig(),
+  testEnvironment: 'node',
+  testMatch: ['**/*.(test|spec).(t|j)s'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/config/', '<rootDir>/mock/'],
+  testTimeout: 30_000,
+  transformIgnorePatterns: ['/node_modules/(?!)'],
+  modulePathIgnorePatterns: [],
+  setupFiles: [],
   transform: {
     '^.+\\.(t|j)s$': transform,
     '^.+\\.mjs$': transform,
   },
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/(.*)mock(.*)/', '<rootDir>/src/antlr4'],
+  coverageReporters: ['json', 'text-summary', 'html'],
 };
